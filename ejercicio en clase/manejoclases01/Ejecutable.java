4b7715b81f08797b7d1908659a9dcd25e0ae9fbd
nicole <maria8nicole@hotmail.com>
2024-04-23 14:54:46 -0500

    clase3

---
+++ ejercicio en clase/manejoclases01/Ejecutable.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejoclases01;
+
+
+public class Ejecutable {
+    public static void main(String[] args) {
+        // Se crea un objeto
+        Hospital hospital = new Hospital();
+        
+        // Se asigna valores particulares al objeto
+        hospital.nombre = "Clinica San Agutin";
+        hospital.numeroCamas = 200;
+        hospital.presupuesto = 560340.43;
+        
+        // Se presenta valores en pantalla
+        System.out.printf("%s - %d - %.1f\n", hospital.nombre,
+                hospital.numeroCamas, hospital.presupuesto);
+        
+        // System.out.println(hospital.nombre + "-" + hospital.numeroCamas);
+    }
+}
